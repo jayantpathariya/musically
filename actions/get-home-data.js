c5db07120283a461c9101d4d1d1311f7faab15cc
Jayant Pathariya <jayantmehra02@gmail.com>
2024-01-24 20:03:06 +0530

    added ? on map

--- actions/get-home-data.js
+++ actions/get-home-data.js
@@ -8,12 +8,12 @@ export const getHomeData = async () => {
     let data = null;
     if (response?.data) {
       data = {
-        new_trending: response?.data?.new_trending.map((item) => ({
+        new_trending: response?.data?.new_trending?.map((item) => ({
           id: item?.id,
           title: item?.title,
           subtitle: item?.subtitle,
           type: item?.type,
-          image: item?.image.replace("150x150", "500x500"),
+          image: item?.image?.replace("150x150", "500x500"),
           year: item?.year,
           link: item?.perma_url.split("/").pop(),
           play_count: item?.play_count,
@@ -22,7 +22,7 @@ export const getHomeData = async () => {
           list: item?.list,
           more_info: item?.more_info,
         })),
-        top_playlists: response?.data?.top_playlists.map((item) => ({
+        top_playlists: response?.data?.top_playlists?.map((item) => ({
           id: item?.id,
           title: item?.title,
           subtitle: item?.subtitle,
@@ -31,7 +31,7 @@ export const getHomeData = async () => {
           image: item?.image.replace("150x150", "500x500"),
           more_info: item?.more_info,
         })),
-        new_albums: response?.data?.new_albums.map((item) => ({
+        new_albums: response?.data?.new_albums?.map((item) => ({
           id: item?.id,
           title: item?.title,
           subtitle: item?.subtitle,
@@ -45,7 +45,7 @@ export const getHomeData = async () => {
           list: item?.list,
           more_info: item?.more_info,
         })),
-        browse_discover: response?.data?.browse_discover.map((item) => ({
+        browse_discover: response?.data?.browse_discover?.map((item) => ({
           id: item?.id,
           title: item?.title,
           subtitle: item?.subtitle,
@@ -54,7 +54,7 @@ export const getHomeData = async () => {
           image: item?.image.replace("150x150", "500x500"),
           more_info: item?.more_info,
         })),
-        charts: response?.data?.charts.map((item) => ({
+        charts: response?.data?.charts?.map((item) => ({
           id: item?.id,
           title: item?.title,
           link: item?.perma_url.split("/").pop(),
@@ -62,7 +62,7 @@ export const getHomeData = async () => {
           image: item?.image.replace("150x150", "500x500"),
           count: item?.count,
         })),
-        radio: response?.data?.radio.map((item) => ({
+        radio: response?.data?.radio?.map((item) => ({
           id: item?.id,
           title: item?.title,
           subtitle: item?.subtitle,
@@ -70,7 +70,7 @@ export const getHomeData = async () => {
           type: item?.type,
           image: item?.image.replace("150x150", "500x500"),
         })),
-        artist_recos: response?.data?.artist_recos.map((item) => ({
+        artist_recos: response?.data?.artist_recos?.map((item) => ({
           id: item?.id,
           title: item?.title,
           subtitle: item?.subtitle,
@@ -79,7 +79,7 @@ export const getHomeData = async () => {
           image: item?.image.replace("150x150", "500x500"),
           more_info: item?.more_info,
         })),
-        tag_mixes: response?.data?.tag_mixes.map((item) => ({
+        tag_mixes: response?.data?.tag_mixes?.map((item) => ({
           id: item?.id,
           title: item?.title,
           subtitle: item?.subtitle,
