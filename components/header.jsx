5ad10b37a8f216d6df525b99b1d17343f8252e13
Jayant Pathariya <jayantmehra02@gmail.com>
2024-01-26 20:43:00 +0530

    header scroll fixed

--- components/header.jsx
+++ components/header.jsx
@@ -34,7 +34,7 @@ export const Header = ({ scrolled, bgColor }) => {
   return (
     <header
       className={cn(
-        "p-4 flex items-center justify-between sticky left-0 top-0 z-20",
+        "p-4 flex items-center justify-between sticky left-0 top-0 z-50",
         scrolled &&
           "flex items-center justify-between sticky left-0 top-0 z-20 transition duration-300",
         scrolled && bgColor
